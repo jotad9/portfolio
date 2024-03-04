7939653d3657ab035027a6a6a600bda2607d9382
jotad9 <jotadaniel2003@gmail.com>
2024-03-05 00:13:50 +0100

    json para tener la página en inglés

--- src/portfolio/index.js
+++ src/portfolio/index.js
@@ -10,6 +10,8 @@ const hamburger = document.querySelector('.hamburger')
 const navList = document.querySelector('.nav-list')
 const socials = document.querySelector('.social-nav')
 const navLinks = document.querySelectorAll('.nav-links')
+const idiomas= document.getElementById('idiomas')
+const textToChange= document.querySelectorAll('[data-section]')
 
 const toggleMobileMenu = () => {
 	hamburger.classList.toggle('open')
@@ -32,9 +34,21 @@ backDeLay:60,
 loop:true
 });
 
-let typed2=new Typed(".intro",{
-	strings:["Soy un apasionado de la programación, me encanta aprender y tener retos que me ayuden a crecer como profesional."],
-	typeSpeed:10,
-	backSpeed:60,
-	backDeLay:60,
-});
+
+
+const cambiarIdioma= async language=>{
+	const request= await fetch(`./idiomas/${language}.json`);
+	const data= await request.json();
+	
+	for(const element of textToChange){
+		const value=element.dataset.value;
+		const section=element.dataset.section;
+		element.innerHTML=data[section][value];
+	}
+
+}
+
+idiomas.addEventListener('click',(e)=>{
+	cambiarIdioma(e.target.parentElement.dataset.language);
+	typed2.strings=["Soy un apasionado de la programación, me encanta aprender y tener retos que me ayuden a creceaaar como profesional."];
+})
